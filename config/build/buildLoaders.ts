f50bef16151e85b501622a1113e75a3d9eef420f
Aleksandr Smirnov <smirnov4970@gmail.com>
2023-04-17 12:05:04 +0700

    add react and css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,35 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export default function (): webpack.RuleSetRule[] {
+export default function ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+
+    const tsLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: /\.module\./,
+                        localIdentName: isDev
+                            ? "[path][name]__[local]--[hash:base64:5]"
+                            : "[hash:base64:5]",
+                    },
+                },
+            },
+            "sass-loader",
+        ]
+    }
     return [
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        },
+        tsLoader,
+        cssLoader,
     ]
 }
