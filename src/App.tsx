321311f756cdf5085a16583964858c43c39cd797
Aleksandr Smirnov <smirnov4970@gmail.com>
2023-04-19 09:59:04 +0700

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import {Link, Route, Routes} from 'react-router-dom'
 import {Suspense} from "react";
 import {MainPageAsync} from "./pages/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
+
+
 
 export const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Theme: {theme}</button>
             <Link to={'/'}>MainPage</Link>
             <Link to={'/about'}>AboutPage</Link>
             <Suspense fallback={<h1>Loading...</h1>}>
