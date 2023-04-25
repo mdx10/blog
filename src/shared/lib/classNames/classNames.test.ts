205118d29e60ac0d6d9234f08693afebbc4728ae
Aleksandr Smirnov <smirnov4970@gmail.com>
2023-04-25 20:11:54 +0700

    add jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,34 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('correct class no mods or additional classes are provided', () => {
+        expect(classNames('foo')).toBe('foo');
+    });
+
+    test('correct class name with additional classes', () => {
+        expect(classNames('foo', {}, ['bar', 'baz'])).toBe('foo bar baz');
+    });
+
+    test('correct class name with mods', () => {
+        const mods:any = {
+            'foo--active': true,
+            'foo--hidden': false,
+            'foo--large': 'yes',
+            'foo--red': null,
+            'foo--blue': undefined,
+        };
+        expect(classNames('foo', mods)).toBe('foo foo--active foo--large');
+    });
+
+    test('correct class name with mixed input', () => {
+        const mods:any = {
+            'foo--active': true,
+            'foo--hidden': false,
+            'foo--large': 'yes',
+            'foo--red': null,
+            'foo--blue': undefined,
+        };
+        expect(classNames('foo', mods, ['bar', 'baz']))
+            .toBe('foo bar baz foo--active foo--large');
+    });
+});
