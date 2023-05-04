c9bebcc80d40d2cfecb0a40a88b9630a48880dc5
Aleksandr Smirnov <smirnov4970@gmail.com>
2023-05-04 22:11:24 +0700

    add menu in collapsed sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,6 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
-// import './styles/index.scss';
 
 export const App = () => {
     const { theme } = useTheme();
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -5,15 +5,20 @@ import ChevronIcon from 'shared/assets/icons/right-chevron.svg';
 import HomeIcon from 'shared/assets/icons/home-icon.svg';
 import InfoIcon from 'shared/assets/icons/info-icon.svg';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import styles from './Sidebar.module.scss';
 
 interface SidebarProps {
     className?: string;
 }
-export const Sidebar = ({ className }: SidebarProps) => {
-    // const { t } = useTranslation();
-    const [collapsed, setCollapsed] = useState(false);
 
+export const Sidebar = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
+    const linkClassName = (active: boolean) => (
+        classNames(styles.navLink, {
+            [styles.navLinkActive]: active,
+        })
+    );
     const onToggle = () => setCollapsed((prev) => !prev);
     return (
         <div
@@ -21,11 +26,19 @@ export const Sidebar = ({ className }: SidebarProps) => {
             className={classNames(styles.sidebar, { [styles.collapsed]: collapsed }, [className])}
         >
             <nav className={styles.nav}>
-                <AppLink to="/" className={styles.navLink} theme={AppLinkTheme.INVERT}>
+                <AppLink
+                    to={RoutePath.main}
+                    className={linkClassName(false)}
+                    theme={AppLinkTheme.INVERT}
+                >
                     <HomeIcon className={styles.navIcon} />
                     <span>MainPage</span>
                 </AppLink>
-                <AppLink to="/about" className={styles.navLink} theme={AppLinkTheme.INVERT}>
+                <AppLink
+                    to={RoutePath.about}
+                    className={linkClassName(false)}
+                    theme={AppLinkTheme.INVERT}
+                >
                     <InfoIcon className={styles.navIcon} />
                     <span>AboutPage</span>
                 </AppLink>
@@ -38,12 +51,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 size={SizeButton.L}
                 square
             >
-                <ChevronIcon
-                    className={classNames(
-                        styles.buttonIcon,
-                        { [styles.buttonIconCollapsed]: collapsed },
-                    )}
-                />
+                <ChevronIcon className={classNames(styles.buttonIcon)} />
             </Button>
         </div>
     );
