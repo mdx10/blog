f0e229cc0f0c154c33cb89f85e0371c788ded977
Aleksandr Smirnov <smirnov4970@gmail.com>
2023-08-30 18:18:52 +0700

    migrate react 18

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,9 +1,13 @@
-import React, { FC, useMemo, useState } from 'react';
+import React, { ReactNode, useMemo, useState } from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
+
+interface ThemeProviderProps {
+    children: ReactNode;
+}
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-const ThemeProvider: FC = ({ children }) => {
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme);
     const defaultProps = useMemo(() => ({
         theme,
--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { useDispatch, useStore } from 'react-redux';
 import { ReduxStoreWithManager, StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema';
 import { Reducer } from '@reduxjs/toolkit';
@@ -9,8 +9,9 @@ export type ReducersList = {
 interface DynamicModuleLoaderProps {
     reducers: ReducersList;
     notRemove?: boolean;
+    children?: ReactNode;
 }
-export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
+export const DynamicModuleLoader = (props: DynamicModuleLoaderProps) => {
     const { children, reducers, notRemove } = props;
     const dispatch = useDispatch();
     const store = useStore() as ReduxStoreWithManager;
